0ab7cda8a16ffcb15688ccf9f8c34bee56cb19b4
kayodeolowo <officialkayodeolowo@gmail.com>
2023-08-16 12:17:21 +0100

    initials

---
+++ Screen/WelcomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+        <Text> weclome screen </Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+
+const styles = StyleSheet.create({})
